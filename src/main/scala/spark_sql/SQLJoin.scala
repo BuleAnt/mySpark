ce40531f0e7fb7dff33e8b924df4a7004e349ad3
noah <634209276@qq.com>
2017-07-21 16:39:54 +0800

    update

--- src/main/scala/spark_sql/SQLJoin.scala
+++ src/main/scala/spark_sql/SQLJoin.scala
@@ -4,82 +4,82 @@ import org.apache.spark.sql.{DataFrame, SQLContext}
 import org.apache.spark.{SparkConf, SparkContext}
 
 /**
-	* join直接通过DataFrame
-	*/
+  * join直接通过DataFrame
+  */
 object DFJoin {
 
-	def main(args: Array[String]): Unit = {
-		val conf = new SparkConf()
-			.setAppName("SparkSQLwithJoinScala")
-			.setMaster("local")
+  def main(args: Array[String]): Unit = {
+    val conf = new SparkConf()
+      .setAppName("SparkSQLwithJoinScala")
+      .setMaster("local")
 
-		val sc = new SparkContext(conf)
-		val sqlContext = new SQLContext(sc)
+    val sc = new SparkContext(conf)
+    val sqlContext = new SQLContext(sc)
 
-		val srcDF = sqlContext.read.format("json").load("hdfs://Master:9000/sparksql/people.json")
+    val srcDF = sqlContext.read.format("json").load("hdfs://Master:9000/sparksql/people.json")
 
-		val peopleJson = Array("{\"name\":\"Michael\",\"age\":20}",
-			"{\"name\":\"Andy\",\"age\":17}",
-			"{\"name\":\"Justin\",\"age\":19}")
-		val joinRDD = sc.parallelize(peopleJson)
-		val destDF = sqlContext.read.json(joinRDD)
+    val peopleJson = Array("{\"name\":\"Michael\",\"age\":20}",
+      "{\"name\":\"Andy\",\"age\":17}",
+      "{\"name\":\"Justin\",\"age\":19}")
+    val joinRDD = sc.parallelize(peopleJson)
+    val destDF = sqlContext.read.json(joinRDD)
 
-		secondJoin(srcDF, destDF)
-	}
+    secondJoin(srcDF, destDF)
+  }
 
-	def secondJoin(sourceDF: DataFrame, destDF: DataFrame): Unit = {
-		sourceDF.registerTempTable("people")
-		destDF.registerTempTable("joinPeople")
-		// 直接对DataFrame进行join
-		val result = sourceDF.filter(sourceDF("score") > 90).join(destDF, sourceDF("name") === destDF("name"), "inner")
-		val rdd = result.map(row => row.getAs[String]("name") + ": " + row.getAs[String]("age") + ": " + row.getAs[String]("score"))
-		rdd.foreach(println)
-	}
+  def secondJoin(sourceDF: DataFrame, destDF: DataFrame): Unit = {
+    sourceDF.registerTempTable("people")
+    destDF.registerTempTable("joinPeople")
+    // 直接对DataFrame进行join
+    val result = sourceDF.filter(sourceDF("score") > 90).join(destDF, sourceDF("name") === destDF("name"), "inner")
+    val rdd = result.map(row => row.getAs[String]("name") + ": " + row.getAs[String]("age") + ": " + row.getAs[String]("score"))
+    rdd.foreach(println)
+  }
 
 }
 
 object DF2RDDJoin {
 
-	case class Person(name: String, score: Int, age: Int)
+  case class Person(name: String, score: Int, age: Int)
 
-	def main(args: Array[String]) {
+  def main(args: Array[String]) {
 
-		val sc = new SparkContext(new SparkConf().setAppName("DF2RDDJoin").setMaster("local"))
-		val sqlContext = new SQLContext(sc)
+    val sc = new SparkContext(new SparkConf().setAppName("DF2RDDJoin").setMaster("local"))
+    val sqlContext = new SQLContext(sc)
 
-		//read(.json)-->DataFrame-->register-->sql-->DF
-		//-->.rdd()-->rowRDD-->map(row(0).collect-->Array[Any]
-		val peopleScoreDF = sqlContext.read.format("json").load("src/main/resources/my_peoples.json")
-		peopleScoreDF.registerTempTable("personScores")
-		val scoresDF = sqlContext.sql("select name,score from personScores where score > 90")
-		val scoresArray = scoresDF.rdd.map(row => row(0)).collect()
+    //read(.json)-->DataFrame-->register-->sql-->DF
+    //-->.rdd()-->rowRDD-->map(row(0).collect-->Array[Any]
+    val peopleScoreDF = sqlContext.read.format("json").load("src/test/resources/my_peoples.json")
+    peopleScoreDF.registerTempTable("personScores")
+    val scoresDF = sqlContext.sql("select name,score from personScores where score > 90")
+    val scoresArray = scoresDF.rdd.map(row => row(0)).collect()
 
-		//jsonArr-->JsonRDD-->read.json(JsonRDD)-->DF
-		val peopleInfo = Array("{\"name\":\"Michael\",\"age\":20}", "{\"name\":\"Andy\",\"age\":17}", "{\"name\":\"Justin\",\"age\":19}")
-		val peopleInfoRDD = sc.parallelize(peopleInfo)
-		val peopleInfoDF = sqlContext.read.json(peopleInfoRDD)
-		peopleInfoDF.registerTempTable("peopleInfo")
+    //jsonArr-->JsonRDD-->read.json(JsonRDD)-->DF
+    val peopleInfo = Array("{\"name\":\"Michael\",\"age\":20}", "{\"name\":\"Andy\",\"age\":17}", "{\"name\":\"Justin\",\"age\":19}")
+    val peopleInfoRDD = sc.parallelize(peopleInfo)
+    val peopleInfoDF = sqlContext.read.json(peopleInfoRDD)
+    peopleInfoDF.registerTempTable("peopleInfo")
 
-		//foreach Array-->sqlStr
-		var sqlStr = "select name,age from peopleInfo where name in ("
-		for (i <- 0 to scoresArray.length - 2) {
-			sqlStr += ("'" + scoresArray(i) + "',")
-		}
-		sqlStr += "'" + scoresArray(scoresArray.length - 1) + "')"
-		println(sqlStr)
-		val nameAgeDF = sqlContext.sql(sqlStr)
+    //foreach Array-->sqlStr
+    var sqlStr = "select name,age from peopleInfo where name in ("
+    for (i <- 0 to scoresArray.length - 2) {
+      sqlStr += ("'" + scoresArray(i) + "',")
+    }
+    sqlStr += "'" + scoresArray(scoresArray.length - 1) + "')"
+    println(sqlStr)
+    val nameAgeDF = sqlContext.sql(sqlStr)
 
-		val nameScoresRDD = scoresDF.rdd.map(row => (row.getAs("name").asInstanceOf[String], row.getAs("score").asInstanceOf[Long]))
-		val nameAgeRDD = nameAgeDF.rdd.map(row => (row.getAs("name").asInstanceOf[String], row.getAs("age").asInstanceOf[Long]))
+    val nameScoresRDD = scoresDF.rdd.map(row => (row.getAs[String]("name"), row.getAs[Long]("score")))
+    val nameAgeRDD = nameAgeDF.rdd.map(row => (row.getAs[String]("name"), row.getAs[Long]("age")))
 
-		//rdd join
-		val resultRDD = nameAgeRDD.join(nameScoresRDD)
-		//rdd to DataFrame
-		import sqlContext.implicits._
-		//value部分顺序与join先后对应,这里为:[name,(age,score)]
-		val resultDF = resultRDD.map(row => Person(row._1, row._2._1.asInstanceOf[Int], row._2._2.asInstanceOf[Int])).toDF
+    //rdd join
+    val resultRDD = nameAgeRDD.join(nameScoresRDD)
+    //rdd to DataFrame
+    import sqlContext.implicits._
+    //value部分顺序与join先后对应,这里为:[name,(age,score)]
+    val resultDF = resultRDD.map(row => Person(row._1, row._2._1.asInstanceOf[Int], row._2._2.asInstanceOf[Int])).toDF
 
-		resultDF.show
-	}
+    resultDF.show
+  }
 
 }
